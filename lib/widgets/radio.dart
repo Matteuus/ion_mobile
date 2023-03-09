6107381749b20d2ee7637f5739ccd42f2faaf505
cadumeneses <carloseduardoms646@gmail.com>
2023-03-09 11:20:02 -0300

    feat: adicionado cores de hover e focus, e adicionando tamanho como paramentro opcional no construtor

--- lib/widgets/radio.dart
+++ lib/widgets/radio.dart
@@ -3,219 +3,238 @@ import 'package:ion_mobile/design/colors.dart';
 import 'package:ion_mobile/design/typography/body.dart';
 import 'package:ion_mobile/design/typography/text_style.dart';
 
-
 abstract class IonRadio<T> extends StatefulWidget {
- final T value;
- final T? groupValue;
- final String label;
- final bool selected;
- final Color color;
- final bool disabled;
- final ValueChanged<T?> onChanged;
- final Color borderColor;
- final IonTextStyle labelStyle;
- final List<IonRadioButton> items;
+  final T value;
+  final T? groupValue;
+  final String label;
+  final Color color;
+  final bool disabled;
+  final ValueChanged<T?> onChanged;
+  final Color borderColor;
+  final IonTextStyle labelStyle;
+  final List<IonRadioButton> items;
+  final Color hoverColor;
+  final Color focusColor;
+  final Color fillColor;
+  final double size;
+  final Color focusBorderColor;
+  final Color focusFillBorderColor;
+  final bool autoFocus;
+  final Color hoverBorderColor;
 
+  const IonRadio({
+    required this.value,
+    super.key,
+    this.groupValue,
+    this.label = '',
+    this.color = IonMainColors.primary6,
+    this.disabled = false,
+    required this.onChanged,
+    this.borderColor = IonMainColors.neutral5,
+    this.labelStyle = const IonTextStyleBody(
+      ionFontStyle: IonFontStyle.normal,
+      ionFontWeight: IonFontWeight.regular,
+      ionBodyColor: IonBodyColor.neutral7,
+    ),
+    this.items = const [],
+    this.hoverColor = IonMainColors.primary1,
+    this.focusColor = IonMainColors.primary5,
+    this.fillColor = IonMainColors.neutral1,
+    this.size = 24,
+    this.focusFillBorderColor = IonMainColors.primary5,
+    this.autoFocus = false,
+    this.focusBorderColor = IonMainColors.primary3,
+    this.hoverBorderColor = IonMainColors.primary3,
+  });
 
- const IonRadio({
-   required this.value,
-   super.key,
-   this.groupValue,
-   this.label = '',
-   this.selected = false,
-   this.color = IonMainColors.primary6,
-   this.disabled = false,
-   required this.onChanged,
-   this.borderColor = IonMainColors.neutral5,
-   this.labelStyle = const IonTextStyleBody(
-     ionFontStyle: IonFontStyle.normal,
-     ionFontWeight: IonFontWeight.regular,
-     ionBodyColor: IonBodyColor.neutral7,
-   ),
-   this.items = const [],
- });
-
-
- @override
- State<IonRadio<T>> createState() => _IonRadioState<T>();
+  @override
+  State<IonRadio<T>> createState() => _IonRadioState<T>();
 }
-
 
 class _IonRadioState<T> extends State<IonRadio<T>> {
- bool hasFocus = false;
- bool isHovering = false;
- Color fillColor = IonMainColors.neutral1;
+  bool hasFocus = false;
+  bool isHovering = false;
 
+  late FocusNode focusNode;
 
- late FocusNode focusNode;
+  @override
+  void initState() {
+    super.initState();
+    focusNode = FocusNode();
+    focusNode.addListener(() {
+      setState(() {
+        hasFocus = focusNode.hasFocus;
+      });
+    });
+  }
 
+  @override
+  void dispose() {
+    focusNode.dispose();
+    super.dispose();
+  }
 
- @override
- void initState() {
-   super.initState();
-   focusNode = FocusNode();
-   focusNode.addListener(() {
-     setState(() {
-       hasFocus = focusNode.hasFocus;
-     });
-   });
- }
+  void onHover(bool hover) {
+    setState(() {
+      isHovering = hover;
+    });
+  }
 
+  Color getColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.pressed,
+      MaterialState.hovered,
+      MaterialState.focused,
+    };
+    if (states.any(interactiveStates.contains)) {
+      return Colors.blue;
+    }
+    return Colors.red;
+  }
 
- @override
- void dispose() {
-   focusNode.dispose();
-   super.dispose();
- }
-
-
- void onHover(bool hover) {
-   isHovering = hover;
-   if (hover) {
-     setState(() {
-       fillColor = IonMainColors.primary1;
-     });
-   } else {
-     setState(() {
-       fillColor = IonMainColors.neutral1;
-     });
-   }
- }
-
-
- @override
- Widget build(BuildContext context) {
-   return Row(
-     mainAxisSize: MainAxisSize.min,
-     children: [
-       InkWell(
-         onTap: widget.disabled
-             ? null
-             : () {
-                 if (widget.groupValue != widget.value) {
-                   setState(() => widget.onChanged(widget.value));
-                 }
-               },
-         onHover: widget.disabled ? null : onHover,
-         focusNode: focusNode,
-         overlayColor: MaterialStateProperty.resolveWith<Color?>(
-           (Set<MaterialState> states) {
-             return Colors.transparent;
-           },
-         ),
-         child: Container(
-           height: 24,
-           width: 24,
-           decoration: BoxDecoration(
-             borderRadius: BorderRadius.circular(100),
-             color: widget.disabled
-                 ? Colors.transparent
-                 : widget.groupValue == widget.value && isHovering
-                     ? IonMainColors.primary1
-                     : widget.groupValue == widget.value
-                         ? Colors.transparent
-                         : fillColor,
-             border: Border.all(
-               color: widget.disabled
-                   ? Colors.transparent
-                   : widget.groupValue == widget.value
-                       ? Colors.transparent
-                       : isHovering
-                           ? Colors.transparent
-                           : hasFocus
-                               ? IonMainColors.primary5
-                               : Colors.transparent,
-               width: hasFocus ? 2 : 1,
-             ),
-           ),
-           child: Center(
-             child: Container(
-               height: 16,
-               width: 16,
-               decoration: BoxDecoration(
-                 borderRadius: BorderRadius.circular(100),
-                 color: widget.disabled && widget.groupValue == widget.value
-                     ? IonMainColors.neutral2
-                     : widget.disabled
-                         ? IonMainColors.neutral3
-                         : widget.groupValue == widget.value && isHovering
-                             ? IonMainColors.primary5
-                             : widget.groupValue == widget.value
-                                 ? IonMainColors.primary6
-                                 : fillColor,
-                 border: Border.all(
-                   color: (widget.disabled && widget.groupValue == widget.value
-                       ? Colors.transparent
-                       : widget.disabled
-                           ? IonMainColors.neutral4
-                           : widget.groupValue == widget.value
-                               ? Colors.transparent
-                               : hasFocus
-                                   ? IonMainColors.primary3
-                                   : isHovering
-                                       ? IonMainColors.primary3
-                                       : IonMainColors.neutral5),
-                   width: 1,
-                 ),
-               ),
-               child: widget.groupValue == widget.value
-                   ? Center(
-                       child: Container(
-                         height: 8,
-                         width: 8,
-                         decoration: BoxDecoration(
-                           color: widget.disabled
-                               ? IonMainColors.neutral4
-                               : widget.groupValue == widget.value
-                                   ? IonMainColors.primary1
-                                   : IonMainColors.neutral1,
-                           borderRadius: BorderRadius.circular(100),
-                         ),
-                       ),
-                     )
-                   : isHovering
-                       ? Center(
-                           child: Container(
-                             height: 4,
-                             width: 4,
-                             decoration: BoxDecoration(
-                               color: IonMainColors.primary2,
-                               borderRadius: BorderRadius.circular(100),
-                             ),
-                           ),
-                         )
-                       : Container(),
-             ),
-           ),
-         ),
-       ),
-       const SizedBox(width: 12),
-       Text(
-         widget.label,
-         style: widget.disabled
-             ? const IonTextStyleBody(
-                 ionFontWeight: IonFontWeight.regular,
-                 ionFontStyle: IonFontStyle.normal,
-                 ionBodyColor: IonBodyColor.neutral5,
-               )
-             : widget.labelStyle,
-       )
-     ],
-   );
- }
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        InkWell(
+          onTap: widget.disabled
+              ? null
+              : () {
+                  if (widget.groupValue != widget.value) {
+                    setState(() => widget.onChanged(widget.value));
+                  }
+                },
+          onHover: widget.disabled ? null : onHover,
+          focusNode: focusNode,
+          overlayColor: MaterialStateProperty.resolveWith<Color?>(
+            (Set<MaterialState> states) {
+              return Colors.transparent;
+            },
+          ),
+          autofocus: widget.autoFocus,
+          child: Container(
+            height: widget.size,
+            width: widget.size,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: widget.disabled
+                  ? Colors.transparent
+                  : widget.groupValue == widget.value && isHovering
+                      ? IonMainColors.primary1
+                      : widget.groupValue == widget.value
+                          ? Colors.transparent
+                          : isHovering
+                              ? widget.hoverColor
+                              : widget.fillColor,
+              border: Border.all(
+                color: widget.disabled
+                    ? Colors.transparent
+                    : widget.groupValue == widget.value
+                        ? Colors.transparent
+                        : isHovering
+                            ? Colors.transparent
+                            : hasFocus
+                                ? widget.focusFillBorderColor
+                                : Colors.transparent,
+                width: hasFocus ? 2 : 1,
+              ),
+            ),
+            child: Center(
+              child: Container(
+                height: widget.size * 0.66,
+                width: widget.size * 0.66,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: widget.disabled && widget.groupValue == widget.value
+                      ? IonMainColors.neutral2
+                      : widget.disabled
+                          ? IonMainColors.neutral2
+                          : widget.groupValue == widget.value && isHovering
+                              ? IonMainColors.primary5
+                              : widget.groupValue == widget.value
+                                  ? widget.color
+                                  : isHovering
+                                      ? widget.hoverColor
+                                      : IonMainColors.neutral1,
+                  border: Border.all(
+                    color: widget.disabled && widget.groupValue == widget.value
+                        ? Colors.transparent
+                        : widget.disabled
+                            ? IonMainColors.neutral4
+                            : widget.groupValue == widget.value
+                                ? Colors.transparent
+                                : hasFocus
+                                    ? widget.focusBorderColor
+                                    : isHovering
+                                        ? widget.hoverBorderColor
+                                        : widget.borderColor,
+                    width: 1,
+                  ),
+                ),
+                child: widget.groupValue == widget.value
+                    ? Center(
+                        child: Container(
+                          height: widget.size * 0.33,
+                          width: widget.size * 0.33,
+                          decoration: BoxDecoration(
+                            color: widget.disabled
+                                ? IonMainColors.neutral4
+                                : widget.groupValue == widget.value
+                                    ? IonMainColors.primary1
+                                    : IonMainColors.neutral1,
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                        ),
+                      )
+                    : isHovering
+                        ? Center(
+                            child: Container(
+                              height: widget.size * 0.1667,
+                              width: widget.size * 0.1667,
+                              decoration: BoxDecoration(
+                                color: IonMainColors.primary2,
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                            ),
+                          )
+                        : Container(),
+              ),
+            ),
+          ),
+        ),
+        if (widget.label != '') const SizedBox(width: 12),
+        Text(
+          widget.label,
+          style: widget.disabled
+              ? const IonTextStyleBody(
+                  ionFontWeight: IonFontWeight.regular,
+                  ionFontStyle: IonFontStyle.normal,
+                  ionBodyColor: IonBodyColor.neutral5,
+                )
+              : widget.labelStyle,
+        )
+      ],
+    );
+  }
 }
 
-
-class IonRadioButton extends IonRadio {
- const IonRadioButton({
-   super.key,
-   required super.value,
-   super.label,
-   required super.onChanged,
-   required super.groupValue,
-   super.disabled,
- });
+class IonRadioButton<T> extends IonRadio {
+  const IonRadioButton({
+    super.key,
+    required super.value,
+    super.label,
+    required super.onChanged,
+    required super.groupValue,
+    super.disabled,
+    super.borderColor,
+    super.color,
+    super.fillColor,
+    super.focusBorderColor,
+    super.focusColor,
+    super.hoverColor,
+    super.labelStyle,
+    super.size,
+    super.autoFocus,
+  });
 }
-
-
-
